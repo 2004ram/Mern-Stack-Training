047fcf2605370a0cbf647ebdd07776b373a48148
2004ram <mundraram04@gmail.com>
2024-09-24 13:59:44 +0530

    update

---
+++ JS assignment/javascript 6/script.js
@@ -0,0 +1,67 @@
+//+ let obj ={
+//     firstname : "ram",
+//     lastname : "mundra"
+// }
+// let keys =object.keys(obj);
+// console.log(keys);
+// let value =object.values(obj);
+// console.log(value);
+
+// let entries =object.entries(obj);
+// console.log(entries);
+
+// object.freeze(obj);
+// obj.firstname ="roshan"
+// console.log(obj);
+
+// const num={
+//     firstname : "name",
+//     lastname : "lname"
+// }
+// num.firstname ="roshan"
+// num.lastname ="sroshan"
+// console.log(num);
+
+
+// Object.seal(obj);
+
+// obj.firstname ="ug";
+// obj.lastname ="ggv";
+// obj.address ="jaipur";
+// console.log(obj);
+// console.log(obj.hasownproperty());
+
+// string method
+
+// let str ="hello world";
+// let char = str.charAt(6);
+// console.log(char);
+
+// let meStr = str.concat("techno");
+// console.log(meStr);
+
+// let str = "hello world";
+
+// let index = str.indexOf("ld");
+// console.log(index);
+
+// let otp = Math.floor(1000 + Math.random()*90000);
+// console.log(otp);
+
+                                                                                                                                
+// Async js
+
+// let a =20;
+// let b =30;
+// console.log(a=BarProp);
+//  setTimeout(()=>{
+//     console.log("hello world");
+
+    
+//  },2000);
+
+//  console.log(a);
+//  console.log(b);
+ 
+
+ 
